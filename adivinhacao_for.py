f5255d384dcef1778127a75d368b38da885fb85d
Felipe Delmondes <luis.delmondes@outlook.com>
2020-07-23 20:30:58 -0300

    Novos commits

--- adivinhacao_for.py
+++ adivinhacao_for.py
@@ -1,24 +1,49 @@
+import random
+
 print("**********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("**********************************")
 
-numero_secreto = 42
-tentativas = 3
+
+
+numero_secreto = random.randrange(1,101)
+tentativas = 0
+pontos = 1000
+
+print('Qual o nível de dificuldade?')
+print('1 - Fácil\n2 - Médio\n3 - Difícil')
+
+print (numero_secreto)
+nivel = int(input('Escolha o nível de dificuldade: '))
+
+if(nivel == 1):
+    tentativas = 20
+elif(nivel ==2):
+    tentativas = 10
+else:
+    tentativas = 5
 
 for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada,tentativas))
     numero_usuario = int(input("Digite o número entre 1 e 100: "))
+    
+    if (numero_usuario < 1 or numero_usuario > 100):
+        print('NUMERO INVALIDO, Favor digitar um número entre 1 e 100')
+        continue
+    
     acertou = numero_secreto == numero_usuario
     maior = numero_usuario > numero_secreto
     menor = numero_usuario < numero_secreto
 
     if (acertou):
-        print("Você acertou o número da máquina: ",numero_secreto)
+        print("Você acertou, o numero é {} e fez {} pontos: ".format(numero_secreto,pontos))
         break
-    elif (maior):
-        print("Você errou, seu número é maior que o número secreto")
     else:
-        print("Você errou, seu número é menor que o número secreto")
+        if(maior):
+            print('Você errou! O seu chute foi maior que o número secreto')
+        elif(menor):
+            print('Você erro! O seu chute foi menor que o número secreto')
+        pontos_perdidos = abs(numero_secreto - numero_usuario)
+        pontos = pontos_perdidos
 
 print("Fim de jogo")
-
