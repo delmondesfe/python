356f9ecdd9ed4bd38783eb23e7d49c1fc34499b8
Felipe <Delmondes>
2020-12-22 14:22:09 -0300

    update

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,50 +1,54 @@
 import random
+
 def jogar():
-    print("**********************************")
-    print("Bem vindo ao jogo de adivinhação")
-    print("**********************************")
 
-
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
     numero_secreto = random.randrange(1,101)
-    tentativas = 0
+    total_de_tentativas = 0
     pontos = 1000
 
-    print('Qual o nível de dificuldade?')
-    print('1 - Fácil\n2 - Médio\n3 - Difícil')
+    print("Qual nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-    print (numero_secreto) #mostrando o número random
-
-    nivel = int(input('Escolha o nível de dificuldade: '))
+    nivel = int(input("Defina o nível: "))
 
     if(nivel == 1):
-        tentativas = 20
-    elif(nivel ==2):
-        tentativas = 10
+        total_de_tentativas = 20
+    elif(nivel == 2):
+        total_de_tentativas = 10
     else:
-        tentativas = 5
+        total_de_tentativas = 5
 
-    for rodada in range(1, tentativas + 1):
-        print("Tentativa {} de {}".format(rodada,tentativas))
-        numero_usuario = int(input("Digite o número entre 1 e 100: "))
-    
-        if (numero_usuario < 1 or numero_usuario > 100):
-            print('NUMERO INVALIDO, Favor digitar um número entre 1 e 100')
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
+        chute_str = input("Digite um número entre 1 e 100: ")
+        print("Você digitou " , chute_str)
+        chute = int(chute_str)
+
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100!")
             continue
-    
-        acertou = numero_secreto == numero_usuario
-        maior = numero_usuario > numero_secreto
-        menor = numero_usuario < numero_secreto
 
-        if (acertou):
-            print("Você acertou, o numero é {} e fez {} pontos: ".format(numero_secreto,pontos))
+        acertou = chute == numero_secreto
+        maior   = chute > numero_secreto
+        menor   = chute < numero_secreto
+
+        if(acertou):
+            print("Você acertou e fez {} pontos!".format(pontos))
             break
         else:
             if(maior):
-                print('Você errou! O seu chute foi maior que o número secreto')
+                print("Você errou! O seu chute foi maior do que o número secreto.")
             elif(menor):
-                print('Você erro! O seu chute foi menor que o número secreto')
-            pontos_perdidos = abs(numero_secreto - numero_usuario)
-            pontos = pontos_perdidos
+                print("Você errou! O seu chute foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
 
-    print("Fim de jogo")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
--- forca.py
+++ forca.py
@@ -1,2 +1,32 @@
 def jogar():
-    print('Bem vindo ao jogo da forca')
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+    
+    palavra_secreta = "python"
+    letras_acertadas = ["_","_","_","_","_","_"]
+    enforcou = False
+    acertou = False
+    
+    print("A palavra secreta contem {} letras".format(len(letras_acertadas)))
+    print(letras_acertadas)
+    
+    while (not enforcou and not acertou ):
+        
+        chute = input ("Qual a letra?: ")
+        chute = chute.strip()
+        
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+                   
+        
+        
+        print(letras_acertadas)
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
